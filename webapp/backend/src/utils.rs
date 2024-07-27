39be94af04c82ae3d5e2ccae337ce3db0a110352
Jay Takahashi (42ubuntu) <qsakura14555@gmail.com>
2024-07-28 08:08:51 +0900

    整形

--- webapp/backend/src/utils.rs
+++ webapp/backend/src/utils.rs
@@ -18,7 +18,7 @@ pub fn generate_session_token() -> String {
 
 pub fn hash_password(password: &str) -> Result<String, AppError> {
     let password_bytes = password.as_bytes();
-    
+
     // Use SHA-1 for hashing
     let mut hasher = Sha1::new();
     hasher.update(password_bytes);
@@ -32,7 +32,7 @@ pub fn hash_password(password: &str) -> Result<String, AppError> {
 
 pub fn verify_password(hashed_password: &str, input_password: &str) -> Result<bool, AppError> {
     let input_password_bytes = input_password.as_bytes();
-    
+
     // Hash the input password and compare
     let input_password_hash = hash_password(input_password)?;
     Ok(hashed_password == input_password_hash)
