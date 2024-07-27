855f86ecb8af6f9185378ab33871f169558e685f
Takumi Hashimoto <thashimo@c2r7s7.42tokyo.jp>
2024-07-27 17:11:24 +0900

    change 0_sample.sql

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -1,11 +1,11 @@
 -- このファイルに記述されたSQLコマンドが、マイグレーション時に実行されます。
 ALTER TABLE users ADD INDEX index_username(username);
-ALTER TABLE users ADD INDEX index_id(id);
 ALTER TABLE users ADD INDEX index_profile_image(profile_image);
 ALTER TABLE sessions ADD INDEX index_session_token(session_token);
 ALTER TABLE sessions ADD INDEX index_session_user_id(user_id);
 ALTER TABLE sessions ADD INDEX index_session_is_valid(is_valid);
-ALTER TABLE locations ADD INDEX index_location_id(id);
+ALTER TABLE locations ADD INDEX index_location_tow_truck_id(tow_truck_id);
+ALTER TABLE location ADD INDEX index_location_timestamp(timestamp);
 ALTER TABLE nodes ADD INDEX index_name(name);
 ALTER TABLE nodes ADD INDEX area_id(area_id);
 ALTER TABLE tow_trucks ADD INDEX index_user_id(user_id);
