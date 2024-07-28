0595fd5fe1b24f9c18231b284e53a41fdc659eb3
Takumi Hashimoto <thashimo@c2r7s7.42tokyo.jp>
2024-07-28 13:32:00 +0900

    add import

--- webapp/backend/src/domains/order_service.rs
+++ webapp/backend/src/domains/order_service.rs
@@ -12,6 +12,8 @@ use crate::{
     models::order::{CompletedOrder, Order},
 };
 
+use futures::future::{ready, join_all, join};
+
 pub trait OrderRepository {
     async fn find_order_by_id(&self, id: i32) -> Result<Order, AppError>;
     async fn update_order_status(&self, order_id: i32, status: &str) -> Result<(), AppError>;
