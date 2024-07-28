e27be6f329712079c25fc048037431d7704f7186
tac4720 <tac472@gmail.com>
2024-07-28 10:53:13 +0900

    Parallel Processing

--- webapp/backend/src/domains/order_service.rs
+++ webapp/backend/src/domains/order_service.rs
@@ -84,6 +84,7 @@ impl<
             .await
     }
 
+/*
     pub async fn get_order_by_id(&self, id: i32) -> Result<OrderDto, AppError> {
         let order = self.order_repository.find_order_by_id(id).await?;
 
@@ -167,6 +168,66 @@ impl<
             completed_time: order.completed_time,
         })
     }
+
+*/
+
+pub async fn get_order_by_id(&self, id: i32) -> Result<OrderDto, AppError> {
+    let order = self.order_repository.find_order_by_id(id).await?;
+    let client_username_future = self.auth_repository.find_user_by_id(order.client_id);
+    let dispatcher_future = match order.dispatcher_id {
+        Some(dispatcher_id) => self.auth_repository.find_dispatcher_by_id(dispatcher_id),
+        None => futures::future::ready(Ok(None)),
+    };
+    let tow_truck_future = match order.tow_truck_id {
+        Some(tow_truck_id) => self.tow_truck_repository.find_tow_truck_by_id(tow_truck_id),
+        None => futures::future::ready(Ok(None)),
+    };
+    let area_id_future = self.map_repository.get_area_id_by_node_id(order.node_id);
+
+    let (client_username, dispatcher, tow_truck, area_id) = futures::join!(
+        client_username_future,
+        dispatcher_future,
+        tow_truck_future,
+        area_id_future
+    );
+
+    let client_username = client_username?.unwrap().username;
+
+    let (dispatcher_user_id, dispatcher_username) = match dispatcher? {
+        Some(dispatcher) => {
+            let user = self.auth_repository.find_user_by_id(dispatcher.user_id).await?.unwrap();
+            (Some(dispatcher.user_id), Some(user.username))
+        },
+        None => (None, None),
+    };
+
+    let (driver_user_id, driver_username) = match tow_truck? {
+        Some(tow_truck) => {
+            let user = self.auth_repository.find_user_by_id(tow_truck.driver_id).await?.unwrap();
+            (Some(tow_truck.driver_id), Some(user.username))
+        },
+        None => (None, None),
+    };
+
+    Ok(OrderDto {
+        id: order.id,
+        client_id: order.client_id,
+        client_username: Some(client_username),
+        dispatcher_user_id,
+        dispatcher_username,
+        driver_user_id,
+        driver_username,
+        area_id: area_id?,
+        dispatcher_id: order.dispatcher_id,
+        tow_truck_id: order.tow_truck_id,
+        status: order.status,
+        node_id: order.node_id,
+        car_value: order.car_value,
+        order_time: order.order_time,
+        completed_time: order.completed_time,
+    })
+}
+
 
     pub async fn get_paginated_orders(
         &self,
