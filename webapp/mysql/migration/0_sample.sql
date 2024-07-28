15c921155a91a8e34866c02c2c2134ae72019365
Jay Takahashi (42ubuntu) <qsakura14555@gmail.com>
2024-07-28 12:47:56 +0900

    del index from completed_orders

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -1,6 +1,6 @@
 -- このファイルに記述されたSQLコマンドが、マイグレーション時に実行されます。
 
-ALTER TABLE orders ADD INDEX index_status(status);
+ALTER TABLE orders ADD INDEX index_orders_status(status);
 ALTER TABLE orders ADD INDEX index_node_id(node_id);
 ALTER TABLE orders ADD INDEX index_order_time(order_time);
 ALTER TABLE orders ADD INDEX index_client_id(client_id);
@@ -19,6 +19,9 @@ ALTER TABLE nodes ADD INDEX index_name(name);
 ALTER TABLE nodes ADD INDEX area_id(area_id);
 
 ALTER TABLE tow_trucks ADD INDEX index_area_id(area_id);
-ALTER TABLE tow_trucks ADD INDEX index_status(status);
+ALTER TABLE tow_trucks ADD INDEX index_tow_trucks_status(status);
 
 ALTER TABLE edges ADD INDEX index_weight(weight);
+
+DROP INDEX order_id ON completed_orders;
+DROP INDEX tow_truck_id ON completed_orders;
