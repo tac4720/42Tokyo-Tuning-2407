09a15e6c0c03081ed4cc6b15e06ff93566cb921e
Takumi Hashimoto <thashimo@c2r7s7.42tokyo.jp>
2024-07-28 13:27:29 +0900

    add pararell

--- webapp/backend/src/domains/order_service.rs
+++ webapp/backend/src/domains/order_service.rs
@@ -95,18 +95,53 @@ impl<
             .await
     }
 
-pub async fn get_order_by_id(&self, id: i32) -> Result<OrderDto, AppError> {
-    let order = self.order_repository.get_order_by_id(id).await?;
+    pub async fn get_order_by_id(&self, id: i32) -> Result<OrderDto, AppError> {
+    let order = self.order_repository.find_order_by_id(id).await?;
+    let client_username_future = self.auth_repository.find_user_by_id(order.client_id);
+    let dispatcher_future = match order.dispatcher_id {
+        Some(dispatcher_id) => self.auth_repository.find_dispatcher_by_id(dispatcher_id),
+        None => futures::future::ready(Ok(None)),
+    };
+    let tow_truck_future = match order.tow_truck_id {
+        Some(tow_truck_id) => self.tow_truck_repository.find_tow_truck_by_id(tow_truck_id),
+        None => futures::future::ready(Ok(None)),
+    };
+    let area_id_future = self.map_repository.get_area_id_by_node_id(order.node_id);
+
+    let (client_username, dispatcher, tow_truck, area_id) = try_join!(
+        client_username_future,
+        dispatcher_future,
+        tow_truck_future,
+        area_id_future
+    )?;
+
+    let client_username = client_username?.unwrap().username;
+
+    let (dispatcher_user_id, dispatcher_username) = match dispatcher? {
+        Some(dispatcher) => {
+            let user = self.auth_repository.find_user_by_id(dispatcher.user_id).await?.unwrap();
+            (Some(dispatcher.user_id), Some(user.username))
+        },
+        None => (None, None),
+    };
+
+    let (driver_user_id, driver_username) = match tow_truck? {
+        Some(tow_truck) => {
+            let user = self.auth_repository.find_user_by_id(tow_truck.driver_id).await?.unwrap();
+            (Some(tow_truck.driver_id), Some(user.username))
+        },
+        None => (None, None),
+    };
 
     Ok(OrderDto {
         id: order.id,
         client_id: order.client_id,
-        client_username: order.client_username,
-        dispatcher_user_id: order.dispatcher_user_id,
-        dispatcher_username: order.dispatcher_username,
-        driver_user_id: order.driver_user_id,
-        driver_username: order.driver_username,
-        area_id: order.area_id,
+        client_username: Some(client_username),
+        dispatcher_user_id,
+        dispatcher_username,
+        driver_user_id,
+        driver_username,
+        area_id: area_id?,
         dispatcher_id: order.dispatcher_id,
         tow_truck_id: order.tow_truck_id,
         status: order.status,
@@ -118,7 +153,7 @@ pub async fn get_order_by_id(&self, id: i32) -> Result<OrderDto, AppError> {
 }
 
 // service
-pub async fn get_paginated_orders(
+ pub async fn get_paginated_orders(
         &self,
         page: i32,
         page_size: i32,
@@ -132,30 +167,62 @@ pub async fn get_paginated_orders(
             .get_paginated_orders_with_details(page, page_size, sort_by, sort_order, status, area)
             .await?;
 
-        let results: Vec<OrderDto> = orders_with_details.into_iter().map(|order| {
-            OrderDto {
+        let futures: Vec<_> = orders_with_details.into_iter().map(|order| async {
+            let client_username_future = self.auth_repository.find_user_by_id(order.client_id);
+            let dispatcher_future = match order.dispatcher_id {
+                Some(dispatcher_id) => self.auth_repository.find_dispatcher_by_id(dispatcher_id),
+                None => future::ready(Ok(None)),
+            };
+            let tow_truck_future = match order.tow_truck_id {
+                Some(tow_truck_id) => self.tow_truck_repository.find_tow_truck_by_id(tow_truck_id),
+                None => future::ready(Ok(None)),
+            };
+
+            let (client_username, dispatcher, tow_truck) = try_join!(
+                client_username_future,
+                dispatcher_future,
+                tow_truck_future
+            )?;
+
+            let client_username = client_username?.unwrap().username;
+            let (dispatcher_user_id, dispatcher_username) = match dispatcher? {
+                Some(dispatcher) => {
+                    let user = self.auth_repository.find_user_by_id(dispatcher.user_id).await?.unwrap();
+                    (Some(dispatcher.user_id), Some(user.username))
+                },
+                None => (None, None),
+            };
+            let (driver_user_id, driver_username) = match tow_truck? {
+                Some(tow_truck) => {
+                    let user = self.auth_repository.find_user_by_id(tow_truck.driver_id).await?.unwrap();
+                    (Some(tow_truck.driver_id), Some(user.username))
+                },
+                None => (None, None),
+            };
+
+            Ok(OrderDto {
                 id: order.id,
                 client_id: order.client_id,
-                client_username: Some(order.client_username),
+                client_username: Some(client_username),
                 dispatcher_id: order.dispatcher_id,
-                dispatcher_user_id: order.dispatcher_user_id,
-                dispatcher_username: order.dispatcher_username,
+                dispatcher_user_id,
+                dispatcher_username,
                 tow_truck_id: order.tow_truck_id,
-                driver_user_id: order.driver_user_id,
-                driver_username: order.driver_username,
+                driver_user_id,
+                driver_username,
                 area_id: order.area_id,
                 status: order.status,
                 node_id: order.node_id,
                 car_value: order.car_value,
                 order_time: order.order_time,
                 completed_time: order.completed_time,
-            }
+            })
         }).collect();
 
-        Ok(results)
-    }
+        let results: Vec<OrderDto> = try_join_all(futures).await?;
 
-    pub async fn create_client_order(
+        Ok(results)
+    }    pub async fn create_client_order(
         &self,
         client_id: i32,
         node_id: i32,
